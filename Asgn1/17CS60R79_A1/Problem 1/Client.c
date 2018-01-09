99b119feb8b1b540946c19592a282c9ae08ed458
Suntron <sumanta.sunny@gmail.com>
2018-01-09 22:30:56 +0530

    Final2.0

---
+++ Asgn1/17CS60R79_A1/Problem 1/Client.c
@@ -0,0 +1,76 @@
+/*############################################################################################################
+# Author:					Sumanta Dey (17CS60R79)
+# Assignment:				Assignment 1
+# Version:					0.2
+# Created Date:				03-Jan-2017
+# Last Modified Date:		09-Jan-2017
+# Last Modified By:			Sumanta Dey
+# Reason of Modification:	Documentation
+###################################### :: Description :: ###############################################################
+# Objective: To creates the client
+############################################################################################################*/
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <netdb.h>
+
+#define PORT 8080
+
+void startClient();
+
+int main(int argc, char const *argv[])
+{
+	startClient();
+	return 0;
+}
+
+void startClient()
+{
+	char buffer[256];
+	int n; 
+	size_t m;
+	int sockfd;
+	struct hostent *server;
+	struct sockaddr_in serv_addr;
+	printf("Starting client.....\n");
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); //Creating the SOCKET
+	//START :: To collect the server details
+	server = gethostbyname("localhost"); //resolves the localhost
+	bzero((char *) &serv_addr, sizeof(serv_addr)); //initialize the server address
+	serv_addr.sin_family = AF_INET; //for IPv4 family
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length); //copy the server(localhost) IP address
+	serv_addr.sin_port = htons(PORT); //defining port number
+	//END :: To collect the server details
+	int status = connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)); //initiating connect request to the server
+	if (status < 0)
+	{
+		printf("ERROR connecting\n");
+		exit(0);
+	}
+	while(1)
+	{
+		memset(buffer, '\0', sizeof(buffer)); //initialize the buffer
+		printf("Client : ");
+		fgets(buffer, sizeof(buffer), stdin); //Take the user input
+		if(buffer[strlen(buffer)-1] == '\n')
+		{
+			buffer[strlen(buffer)-1] = '\0'; //removes the new line character
+		}
+		n = write(sockfd, buffer, strlen(buffer)); //write the user input to the socket
+		if(strcmp(buffer, "close") == 0)
+		{
+			break;
+		}
+		else
+		{
+			memset(buffer, '\0', sizeof(buffer)); //reset the buffer
+			n = read(sockfd,buffer,255); //read from the socket(server message)
+			buffer[n] = '\0';
+			printf("Server : %s\n", buffer);
+		}
+	}
+	close(sockfd); //Close the SOCKET
+}
--- Asgn1/17CS60R79_A1/Problem 1/Server.c
+++ Asgn1/17CS60R79_A1/Problem 1/Server.c
@@ -20,8 +20,6 @@
 #define PORT 8080
 
 void createServer();
-char * readLineFromFile(int lineNo);
-char * appendToTheFile(char * line);
 
 int main(int argc, char const *argv[])
 {
@@ -76,121 +74,11 @@ void createServer()
 		}
 		else
 		{
-			char * strMsg;
-			char msg[10][1024];
-			memset(msg, '\0', 10*1024);
-			int i = 0, j = 0, k = 0, f = 0;
-			//START :: Parse the client message
-			for(i = 0; i < strlen(message); i++)
-			{
-				if(f == 0)
-				{
-					if(message[i] == ' ')
-					{
-						k = 0;
-						j++;
-						f = 1;
-					}
-					else
-					{
-						msg[j][k] = message[i];
-						k++;
-					}
-				}
-				else
-				{
-					msg[j][k] = message[i];
-					k++;
-				}
-			}
-			//START :: Parse the client message
-			if(j == 1)
-			{
-				if(strcmp(msg[0], "READX") == 0)
-				{
-					int lineNo = atoi(msg[1]);
-					strMsg = readLineFromFile(lineNo);
-					sprintf(reply, "%s", strMsg);
-					free(strMsg);
-				}
-				else if (strcmp(msg[0], "WRITEX") == 0)
-				{
-					strMsg = appendToTheFile(msg[1]);
-					sprintf(reply, "%s", strMsg);
-					free(strMsg);
-				}
-				else
-				{
-					sprintf(reply, "wrong operation code");
-				}
-			}
-			else
-			{
-				sprintf(reply, "wrong number of arguments");
-			}
+			sprintf(reply, "%s", "got your message");
 			n = write(newsockfd, reply, strlen(reply));
 			printf("Server : %s\n", reply);
 		}
 	} 
 	close(newsockfd); //Close the Client specific socket
 	close(sockfd);  //Close the Server socket
-}
-
-char * readLineFromFile(int lineNo)
-{
-	FILE * fp;
-	char line[1024];
-	char * lineCopy;
-	lineCopy = (char *)calloc(1024, 1);
-	fp = fopen("data.txt", "r"); //open the data file in read mode
-	if (fp==NULL)
-	{
-		strcpy(lineCopy, "unable to open the file");
-		return lineCopy;
-	}
-	while(fgets(line, sizeof(line), fp))
-	{
-		lineNo--;
-		if(lineNo == 0)
-		{
-			if(line[strlen(line)-1] == '\n')
-			{
-				line[strlen(line)-1] = '\0';
-			}
-			if(strlen(line) == 0)
-			{
-				strcpy(lineCopy, " ");
-			}
-			else
-			{
-				strcpy(lineCopy, line);
-			}
-			fclose(fp);
-			return lineCopy; //return the desired line of the file
-		}
-		else if(lineNo < 0)
-		{
-			break;
-		}
-	}
-	strcpy(lineCopy, "line doesn't exists");
-	fclose(fp); //Close the file
-	return lineCopy;
-}
-
-char * appendToTheFile(char * line)
-{
-	FILE * fp;
-	char * message;
-	message = (char *)calloc(1024, 1);
-	fp = fopen("data.txt", "a"); //Open the file in append mode
-	if(fp==NULL)
-	{
-		strcpy(message, "unable to open the file");
-		return message;
-	}
-	fprintf(fp, "\n%s", line);
-	fclose(fp); //Close the file
-	strcpy(message, "Success!");
-	return message;
 }
